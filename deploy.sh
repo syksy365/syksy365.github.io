f0d3b1998796eff1fe6aa486d19c3b243edfa77c
aboat <clear_windy@yeah.net>
2020-06-17 15:19:23 +0800

    x

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
+yarn docs:build
 
-# 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
 echo 'syksy.site' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://${token}@github.com/syksy365/syksy365.github.io.git master
 
 cd -
